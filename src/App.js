916cb91c074fd987d8c19819d1125a0ac1e8a2f6
rodmirsantana <rodmirandasantana@gmail.com>
2019-07-13 14:54:34 -0300

    React app folder added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const md5 = require('md5');
+
+const publicKey = 'fadb3d6ff2c31aceefc52728631cd643';
+const privateKey = '9a5f1bd4bab1b6c2f5f6ba80b1477e7e03ba416c';
+const timeStamp = Date.now().toString();
+
+const hash = md5(timeStamp + privateKey + publicKey);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
